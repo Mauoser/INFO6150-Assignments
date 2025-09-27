1521903811581843cba434a71f266b7ccb42a5d9
Rei Zhang <mauoser@outlook.com>
2025-09-27 02:53:22 -0400

    updated js

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -3,25 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
         let table = document.getElementById("myTable");
         let tbody = table.querySelector("tbody");
 
-        // need to get last Book row (to get inside)
         let lastRow = tbody.lastElementChild;
-        let bookCell = lastRow?.children[1];
-        let lastRowIndex = bookCell ? parseInt(bookCell.textContent.split(" ")[1]) : 0;
+        let studentCell = lastRow?.children[1];
+        let lastRowIndex = studentCell ? parseInt(studentCell.textContent.split(" ")[1]) : 0;
 
-        // creating the new Row
         let newRow = document.createElement("tr");
         let newCheckboxCell = document.createElement("td");
         newCheckboxCell.innerHTML = `<input type="checkbox" onclick="onClickCheckbox(this)">`;
 
-        let newBookCell = document.createElement("td");
-        newBookCell.textContent = `Student ${parseInt(lastRowIndex) + 1}`;
+        let newStudentCell = document.createElement("td");
+        newStudentCell.textContent = `Student ${parseInt(lastRowIndex) + 1}`;
 
-        let newAuthorCell = document.createElement("td");
-        newAuthorCell.textContent = `Advisor ${parseInt(lastRowIndex) + 1}`;
+        let newAdvisorCell = document.createElement("td");
+        newAdvisorCell.textContent = `Advisor ${parseInt(lastRowIndex) + 1}`;
+
+        let newAwardCell = document.createElement("td");
+        newAwardCell.textContent = `Approved`;
 
         newRow.appendChild(newCheckboxCell);
-        newRow.appendChild(newBookCell);
-        newRow.appendChild(newAuthorCell);
+        newRow.appendChild(newStudentCell);
+        newRow.appendChild(newAdvisorCell);
+        newRow.appendChild(newAwardCell);
         tbody.appendChild(newRow);
     }
 
