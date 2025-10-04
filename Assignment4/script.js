3b7f2e3add1b7901eb9dfb017bec85cbd9dbc0df
Rei Zhang <mauoser@outlook.com>
2025-10-04 00:27:02 -0400

    created validate form function

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -5,5 +5,27 @@ let emailId = document.getElementById("emailId");
 let phoneNumber = document.getElementById("phoneNumber");
 let zipcode = document.getElementById("zipcode");
 let checkbox = document.querySelectorAll('input[type="checkbox"]');
-let feedbackForm = document.getElementById("feedbackForm");
+let form = document.getElementById("feedbackForm");
 let comments = document.getElementById("comments");
+
+let isFirstNameValid = false;
+let isLastNameValid = false;
+let isEmailValid = false;
+let isPhoneValid = false;
+let isZipcode = false;
+let isCommentsValid = false;
+
+function validateForm(event) {
+  event.preventDefault();
+  if (isEmailValid && isNameValid && isPhoneValid) {
+    firstName.value = "";
+    lastName.value = "";
+    emailId.value = "";
+    phoneNumber.value = "";
+    zipcode.value = "";
+    comments.value = "";
+    alert("Form submitted successfully!");
+  } else {
+    alert("Please fill out the form correctly before submitting.");
+  }
+}
