b9379e9f8f51f72f602a24b1108e61fab30dc5ce
Rei Zhang <mauoser@outlook.com>
2025-10-04 00:39:04 -0400

    added validation for first name

--- Assignment4/script.js
+++ Assignment4/script.js
@@ -8,6 +8,16 @@ let checkbox = document.querySelectorAll('input[type="checkbox"]');
 let form = document.getElementById("feedbackForm");
 let comments = document.getElementById("comments");
 
+const regExName = /^[A-Za-z]+(?:\s[A-Za-z]+)*$/;
+const regExEmail = /^[a-zA-Z0-9._%+-]+@northeastern\.edu$/;
+const regExPhone = /\d{3}-?\d{3}-\d{4}$/;
+
+firstName.addEventListener("input", validate);
+lastName.addEventListener("input", validate);
+emailId.addEventListener("input", validate);
+phoneNumber.addEventListener("input", validate);
+form.addEventListener("submit", validateForm);
+
 let isFirstNameValid = false;
 let isLastNameValid = false;
 let isEmailValid = false;
@@ -27,5 +37,26 @@ function validateForm(event) {
     alert("Form submitted successfully!");
   } else {
     alert("Please fill out the form correctly before submitting.");
+  }
+}
+
+function validate(event) {
+  const value = event.target.value;
+  const fieldId = event.target.id;
+  const errorField = `error-${fieldId}`;
+  const error = document.getElementById(errorField);
+
+  if (fieldId === "firstName") {
+    isNameValid = value.trim().match(regExName);
+    error.style.display = isNameValid ? "none" : "block";
+  } else if (fieldId === "lastName") {
+    isEmailValid = value.trim().match(regExName);
+    error.style.display = isEmailValid ? "none" : "block";
+  } else if (fieldId === "emailId") {
+    isEmailValid = value.trim().match(regExEmail);
+    error.style.display = isEmailValid ? "none" : "block";
+  } else if (fieldId === "phoneNumber") {
+    isPhoneValid = value.trim().match(regExPhone);
+    error.style.display = isPhoneValid ? "none" : "block";
   }
 }
