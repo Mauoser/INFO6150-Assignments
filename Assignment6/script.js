8a8fc500e953985c15a17a5409e411f1e05b0572
Rei Zhang <mauoser@outlook.com>
2025-10-24 02:39:01 -0400

    Added hardcoded users to js

---
+++ Assignment6/script.js
@@ -0,0 +1,5 @@
+const USERS = [
+  { email: "zhang.l5@northeastern.edu", password: "password123" },
+  { email: "smith.j@northeastern.edu", password: "password123" },
+  { email: "jones.a@northeastern.edu", password: "password123" },
+];
