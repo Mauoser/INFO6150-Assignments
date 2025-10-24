4e37643f761e969318a7c15c836d775a528a7a79
Rei Zhang <mauoser@outlook.com>
2025-10-24 17:11:27 -0400

    Selected calculator elements with jQuery and stored them in variables

--- Assignment6/calculator.js
+++ Assignment6/calculator.js
@@ -19,4 +19,15 @@ $(document).ready(() => {
     window.location.href = "login.html";
     return;
   }
+
+  $("#welcomeMessage").text(`Welcome, ${session.username}!`);
+
+  const $num1 = $("#num1");
+  const $num2 = $("#num2");
+  const $num1Error = $("#num1Error");
+  const $num2Error = $("#num2Error");
+  const $calcError = $("#calcError");
+  const $result = $("#result");
+  const $ops = $(".op");
+  const $logoutBtn = $("#logoutBtn");
 });
