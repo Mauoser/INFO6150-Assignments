feb56f6d006e8b9745731741e0582d84fe08a63c
Rei Zhang <mauoser@outlook.com>
2025-11-08 03:07:32 -0500

    Created Express server with environment variables

---
+++ Assignment8/server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI =
+  process.env.MONGO_URI || "mongodb://localhost:27017/assignment8";
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
