c644b01427a53997f8303839f536d9f11d20dd7f
Rei Zhang <mauoser@outlook.com>
2025-11-08 03:29:38 -0500

    Added basic Swagger API documentation

--- Assignment8/server.js
+++ Assignment8/server.js
@@ -22,3 +22,19 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Assignment 8 Users API",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./routes/*.js"],
+});
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
