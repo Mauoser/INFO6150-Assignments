32d0048aca5a338ebde1c96dc1f13d3d3dcc0d5d
Rei Zhang <mauoser@outlook.com>
2025-11-08 03:33:03 -0500

    Added Multer middleware for image upload

--- Assignment8/models/User.js
+++ Assignment8/models/User.js
@@ -1,11 +1,12 @@
+// models/User.js
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
     fullName: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true },
-    imagePath: { type: String }, // optional
+    password: { type: String, required: true }, // hashed by bcrypt
+    imagePath: { type: String }, // e.g. /images/filename.ext
   },
   { timestamps: true }
 );
